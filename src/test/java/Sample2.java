f5e79afbc76c66cccf63fcb2baa6838bffc9763c
venkatasubramanian <vkandavelu@hotmail.com>
2021-10-10 21:56:50 +0530

    git master update

--- src/test/java/Sample2.java
+++ src/test/java/Sample2.java
@@ -3,5 +3,6 @@ public class Sample2 {
     public static void main(String[] args) {
         System.out.println("myfirstbrance");
         System.out.println("branch code");
+        System.out.println("branch code");
     }
 }
